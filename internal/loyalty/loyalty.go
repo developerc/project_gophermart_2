1f3658c655a9d24ec610b0f536c347c6f1fe5476
Andrey Saperov <developerc@yandex.ru>
2025-02-18 17:42:48 +0200

    next commit14

--- internal/loyalty/loyalty.go
+++ internal/loyalty/loyalty.go
@@ -3,6 +3,7 @@ package loyalty
 import (
 	"database/sql"
 	"log"
+	"time"
 
 	dbstorage "github.com/developerc/project_gophermart_2/internal/db_storage"
 	"github.com/developerc/project_gophermart_2/internal/general"
@@ -46,13 +47,17 @@ func RunLoyalty2(chSignal general.ChSignal, db *sql.DB, adresAccrual string) {
 				DoRequests(db, chanCnt, arrOrderNumb, adresAccrual)
 			}*/
 			//cancel()
-			<-chSignal.ChStart
-			arrOrderNumb, err := dbstorage.GetOrderNumbs(db)
-			if err != nil {
-				log.Println(err)
-				continue
+			select {
+			case <-chSignal.ChStart:
+				arrOrderNumb, err := dbstorage.GetOrderNumbs(db)
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+				DoRequests(db, chanCnt, arrOrderNumb, adresAccrual, chSignal)
+			case pause := <-chSignal.ChPause:
+				time.Sleep(time.Duration(pause) * time.Second)
 			}
-			DoRequests(db, chanCnt, arrOrderNumb, adresAccrual, chSignal)
 		}
 	}()
 }
